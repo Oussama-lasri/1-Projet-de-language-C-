d15808438168929d0e3631ed97709def66618ce0
ousama lasri <oussama.lasri.2k@gmail.com>
2022-09-09 18:19:23 +0100

    calculatrice C

--- les_operat_avec_if.c
+++ les_operat_avec_if.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    // LES OPERATION
-    // on declarer 2 variable entier
-    float A ,B ;
-    // l'utilisateur doit saisir les deux variables A et B ;
-    printf("saisir A ");
-    scanf("%f",&A);
-    printf("saisir B ");
-    scanf("%f",&B);
-   // choisir un operation
-   char operation ;
-   printf("choisir un opeartion : \n +\n -\n /\n *");
+    // cos / sin / tan
+
+    printf("1- Calcul Cos \n 2- Calcul Sin \n 3-Calcul Tan");
+
+    int X ;
+
+
+
+
 
 
 
