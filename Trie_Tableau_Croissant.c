3931f34ed8690bf160a5be157c08cc48bfeea7de
ousama lasri <oussama.lasri.2k@gmail.com>
2022-09-16 17:23:42 +0100

    gestion de classe

--- Trie_Tableau_Croissant.c
+++ Trie_Tableau_Croissant.c
@@ -1,44 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int ordreCroissant (T){
-    int i , Taille ,ech , j;
-
-    for(i=0 ;i<Taille ; i++){
-            for(j=i+1 ;j<Taille ; j++){
-            if (T[i]>T[j]){
-                ech = T[i];
-                T[i] = T[j];
-                T[j] = ech ;
-                }
-            }
-        return T ;
-
-
-        }
-
-
-
-
-
 
 int main(){
 
-        int Taille ,i,ech ,j;
-        printf("donner la taille de tableau : ");
+        int i,ech ,j;
+        /*printf("donner la taille de tableau : ");
         scanf("%d",&Taille);
         int T[Taille];
         for(i=0 ;i<Taille ; i++){
             printf("donner la valeur %d : ",i+1);
             scanf("%d",&T[i]);
-        }
+        }*/
+         int T[]={6,8,-8,4,9,10,-1,15,33,3};
+        int Taille = 10 ;
+    for(i=0 ;i<Taille ; i++){
+            for(j=i+1 ;j<Taille ; j++){
+                if (T[i]>T[j]){
+                    ech = T[i];
+                    T[i] = T[j];
+                    T[j] = ech ;
+                    }
+            }
 
-        ordreCroissant(T);
+    }
 
-        for(i=0 ;i<Taille ; i++){
-            printf("T[%d] = %d : ",i+1,T[i]);
 
-        }
+
+       printf("apres  le tri \n");
+     for (int k = 0 ; k< Taille ; k++){
+            printf("T[%ld] : %ld \n",k+1,T[k]);
+
+    }
 
 
 
